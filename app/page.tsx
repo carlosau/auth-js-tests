5f35d4fe28948391e982867ee736b74d2ef94b97
Carlos Souza <carlosaugsz@hotmail.com>
2024-01-10 17:12:03 -0300

    added button onclick function

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login-button";
+
 export default function Home() {
   return (
-   
-   <div>
-    <h1>Hello Auth</h1>
-   </div>
-  )
+    <main className="flex h-full flex-col justify-center items-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-orange-300 to-gray-100">
+      <div className="space-y-6 text-center">
+        <h1 className="text-5xl tracking-wide font-semibold text-white drop-shadow-md">👔Auth</h1>
+        <p className="text-lg text-white">Login to access our system</p>
+        <div>
+          <LoginButton>
+          <Button variant="secondary" size="lg">Log In</Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
+  );
 }
