99c5c7742c31a61336e6147bd88a012fea271a23
Carlos Souza <carlosaugsz@hotmail.com>
2024-01-10 18:05:17 -0300

    basic functions

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { LoginButton } from "@/components/auth/login-button";
 
 export default function Home() {
   return (
-    <main className="flex h-full flex-col justify-center items-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-orange-300 to-gray-100">
+    <main className="flex h-full flex-col justify-center items-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-300 to-gray-700">
       <div className="space-y-6 text-center">
         <h1 className="text-5xl tracking-wide font-semibold text-white drop-shadow-md">👔Auth</h1>
         <p className="text-lg text-white">Login to access our system</p>
