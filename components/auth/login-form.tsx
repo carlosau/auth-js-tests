01280197452e9b516563cc4a925fc368dc8ad729
Carlos Souza <carlosaugsz@hotmail.com>
2024-01-10 19:51:41 -0300

    created the card-wrapper for login

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,7 +1,13 @@
+import { CardWrapper } from "./card-wrapper"
+
 export const LoginForm = () => {
     return (
-        <div>
-            Login Form!
-        </div>
+        <CardWrapper
+            headerLabel="Welcome back"
+            backButtonLabel="Don't have an account?"
+            backButtonHref="/auth/register"
+            showSocial>
+                Login Form!
+        </CardWrapper>
     )
 }
