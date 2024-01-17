e941de4d2cacaab2bcfaab7e4f6d49768360ba86
Carlos Souza <carlosaugsz@hotmail.com>
2024-01-17 15:16:53 -0300

    created form inputs with submit button

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,13 +1,86 @@
+"use client"
+
+import * as z from "zod"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { LoginSchema } from "@/schemas"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form"
+import { Input } from "../ui/input"
+import { Button } from "../ui/button"
 import { CardWrapper } from "./card-wrapper"
 
 export const LoginForm = () => {
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        }
+    })
+
+    const OnSubmit = (values: z.infer<typeof LoginSchema>) => {
+        console.log(values)
+    }
+
     return (
         <CardWrapper
             headerLabel="Welcome back"
             backButtonLabel="Don't have an account?"
             backButtonHref="/auth/register"
             showSocial>
-                Login Form!
+                <Form {...form}>
+                    <form
+                    onSubmit={form.handleSubmit(OnSubmit)}
+                    className="space-y-6"
+                    >
+                    <div className="space-y-4">
+                        <FormField 
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input 
+                                        {...field}
+                                        type="email"
+                                        placeholder="youremail@example.com"
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField 
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Senha</FormLabel>
+                                    <FormControl>
+                                        <Input 
+                                        {...field}
+                                        type="password"
+                                        placeholder="******"
+                                        />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </div>
+                    <Button>
+                        Login
+                    </Button>
+                    </form>
+                </Form>
         </CardWrapper>
     )
 }
